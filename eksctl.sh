041e43f94fae40281fb11a9e66e20cb5f032da95
salmakhatun1514 <53810988+salmakhatun4842@users.noreply.github.com>
2024-07-10 20:49:23 +0530

    Update eksctl.sh

--- eksctl.sh
+++ eksctl.sh
@@ -1,6 +1,6 @@
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 
-Move the extracted binary to /usr/local/bin. 
+##Move the extracted binary to /usr/local/bin## 
 
 sudo mv /tmp/eksctl /usr/local/bin
 
